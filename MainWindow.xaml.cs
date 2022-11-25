15ce0acd53b08651e9e5c27b77e146f60f6b409a
HubbaBubba3000 <100069933+HubbaBubba3000@users.noreply.github.com>
2022-11-25 11:27:08 +0300

    Create Window (25.11.22)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WPFZIP
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        // {
+        //     if (e.ChangedButton == MouseButton.Left) 
+        //         this.DragMove();
+        // }
+
+
+    }
+}
